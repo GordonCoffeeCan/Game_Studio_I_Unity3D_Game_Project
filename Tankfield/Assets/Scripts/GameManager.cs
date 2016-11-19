b6ee6abfa967eae7f0dd29c4a2db8cdb786b40e1
GordonCoffeeCan <664220842@qq.com>
2016-11-19 16:51:35 -0500

    PC

---
+++ Tankfield/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+    public static bool gameIsOver;
+
+    public GameObject AlliesText;
+    public GameObject AxisText;
+
+	// Use this for initialization
+	void Start () {
+        gameIsOver = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void AlliesWin() {
+        AlliesText.SetActive(true);
+    }
+
+    public void AxisWin() {
+        AxisText.SetActive(true);
+    }
+}
